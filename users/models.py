94d01d24cc2692d792d9287eada52938021326ff
Mario Albo <mario.albo@fib.upc.edu>
2019-05-11 12:47:12 +0200

    cosas

---
+++ users/models.py
@@ -0,0 +1,7 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class CustomUser(User):
+    level = models.IntegerField()
+    awards = models.CharField(max_length=255)
